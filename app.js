baceeba656fe30b388e9fa6ecd98ff1f3e7d31a2
adamvitaris <126414868+adamvitaris@users.noreply.github.com>
2023-12-11 22:00:20 +0100

    style.css finished
    Started coding app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const num = document.querySelector(".number");
+const dec = document.querySelector(".decrease");
+const reset = document.querySelector(".reset");
+const inc = document.querySelector(".increase");
+let varNum = "";
+
+// TODO
+// Add eventLiesteners and if statements, 
+// make them buttons do some and the counter count.
+// Don't forget the colors.
+
