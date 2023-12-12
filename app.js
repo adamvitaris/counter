7056aeec04c37c5121601d6c09b0408597236fff
adamvitaris <126414868+adamvitaris@users.noreply.github.com>
2023-12-12 20:41:13 +0100

    Update app.js
    Added some functions and finished the app

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const num = document.querySelector(".number");
 const dec = document.querySelector(".decrease");
 const reset = document.querySelector(".reset");
 const inc = document.querySelector(".increase");
-let varNum = "";
+let varNum = 0;
 
-// TODO
-// Add eventLiesteners and if statements, 
-// make them buttons do some and the counter count.
-// Don't forget the colors.
+let num = document.querySelector(".number");
 
+inc.addEventListener("click", function() {
+    varNum ++;
+    num.textContent = varNum;
+    updateColor();
+});
+
+dec.addEventListener("click", function() {
+    varNum--;
+    num.textContent = varNum;
+    updateColor();
+});
+
+reset.addEventListener("click", function() {
+    varNum = 0;
+    num.textContent = varNum;
+    updateColor();
+});
+
+function updateColor() {
+if (varNum < 0) {
+    num.style.color = "#bb2525";
+} else if (varNum > 0) {
+    num.style.color = "#25bb32";
+} else {
+    num.style.color = "#222";
+}}
